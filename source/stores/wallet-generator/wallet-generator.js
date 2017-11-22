9372adf4a8ee6c305eb48b94b7ea87c6eb968255
dev <dev@0v1se.com>
2017-11-23 00:39:10 +0300

    generate wallet on client

---
+++ source/stores/wallet-generator/wallet-generator.js
@@ -0,0 +1,39 @@
+import { observable, computed, action, runInAction } from 'mobx';
+import { Wallet } from 'ethers-wallet';
+
+export class WalletGenerator {
+  @observable generatedWallet = null;
+  @observable encryptedWallet = '';
+  @observable progress = 0;
+
+  @computed get isGenerating() {
+    return this.generatedWallet === null && this.progress > 0;
+  }
+
+  @computed get isGenerated() {
+    return this.generatedWallet !== null;
+  }
+
+  @action generate = ({ password }) => {
+    const randomWallet = Wallet.createRandom({});
+
+    randomWallet
+      .encrypt(password, (progress) => {
+        runInAction(() => {
+          this.progress = progress;
+        });
+      })
+      .then((encryptedWallet) => {
+        console.log(typeof encryptedWallet);
+        runInAction(() => {
+          this.generatedWallet = {
+            ...randomWallet,
+            password,
+          };
+          this.encryptedWallet = encryptedWallet;
+        });
+      });
+  };
+}
+
+export default new WalletGenerator();
