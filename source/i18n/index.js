1d01705932a51ace2462a01bde59223058a07e49
Dev <dev@0v1se.com>
2017-12-05 22:22:32 +0300

    Load locales dynamically

--- source/i18n/index.js
+++ source/i18n/index.js
@@ -1,17 +1,22 @@
 import i18next from 'i18next';
+import xhrBackend from 'i18next-xhr-backend';
 import { reactI18nextModule } from 'react-i18next';
 
 i18next
   .use(reactI18nextModule)
+  .use(xhrBackend)
   .init({
     lng: 'en',
+    fallbackLng: 'en',
+    ns: [
+      'common',
+      'auth',
+      'faq',
+      'payment-methods',
+      'wallet',
+      'widgets',
+    ],
     defaultNS: 'common',
   });
-
-const enLocale = window.config.locales.en;
-
-Object.keys(enLocale).forEach((namespace) => {
-  i18next.addResourceBundle('en', namespace, enLocale[namespace], true, true);
-});
 
 export default i18next;
