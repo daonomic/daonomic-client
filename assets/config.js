2e6b281781769ea2b884420d70329b05c18fff40
dev <dev@0v1se.com>
2018-02-10 23:21:36 +0300

    Set sale depending on NODE_ENV

--- assets/config.js
+++ assets/config.js
@@ -1,6 +1,9 @@
 window.config = Object.assign(window.config || {}, {
   realm: '5a2a67f222781cecae23200e',
-  sale: '0x99a09f0d85bc6e95e110348a8522f98443e31c4a',
+  sale: {
+    development: '0x99a09f0d85bc6e95e110348a8522f98443e31c4a',
+    production: '0x99a09f0d85bc6e95e110348a8522f98443e31c4a',
+  },
   common: {
     contactEmail: 'dev@0v1se.com',
     termsOfServiceURL: 'terms.url',
--- source/config/common.js
+++ source/config/common.js
@@ -1,4 +1,4 @@
 export const { realm } = window.config;
-export const { sale } = window.config;
+export const sale = window.config.sale[process.env.NODE_ENV];
 export const { contactEmail } = window.config;
 export const { termsOfServiceURL } = window.config;
