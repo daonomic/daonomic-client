06555137082de560861a041d7c847fc70881b01c
Dev <dev@0v1se.com>
2018-03-12 15:52:09 +0300

    Refactor root store

--- source/components/mobx-provider/index.js
+++ source/components/mobx-provider/index.js
@@ -2,11 +2,11 @@
 import * as React from 'react';
 import { useStrict } from 'mobx';
 import { Provider } from 'mobx-react';
-import stores from '~/stores';
+import rootStore from '~/stores';
 
 // Set strict mode to forbid changing state outside of an action
 useStrict(true);
 
 export default function MobxProvider(props: {}) {
-  return <Provider {...props} {...stores} />;
+  return <Provider {...props} {...rootStore} />;
 }
