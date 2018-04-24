965d23e64caa874e4a1360f70dd1477ffa2c9ca4
Dev <dev@0v1se.com>
2018-04-24 19:48:28 +0300

    chrome headless

--- wdio.config.js
+++ wdio.config.js
@@ -44,6 +44,12 @@ exports.config = {
       // frequent calls lead to HTTP error 500 responses
       maxInstances: 1,
       browserName: 'chrome',
+      chromeOptions: {
+        // to run chrome headless the following flags are required
+        // (see https://developers.google.com/web/updates/2017/04/headless-chrome)
+        // args: ['--headless', '--disable-gpu'],
+        args: ['--headless', '--disable-gpu'],
+      }
     },
   ],
   //
