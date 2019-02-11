9b1319f15509a7ecd57df931440d282736a2e133
Dev <dev@0v1se.com>
2019-02-11 17:17:10 +0300

    [416] Set sum&substance api url according to current environment

--- source/modules/kyc/sumsub/index.js
+++ source/modules/kyc/sumsub/index.js
@@ -1,12 +1,15 @@
 // @flow
 import { loadScript } from '~/domains/app/assets';
+import { environment } from '~/domains/app/config';
 
 let areAssetsLoaded = false;
 
 export async function initSumsub(): Promise<any> {
   if (!areAssetsLoaded) {
     await loadScript(
-      'https://test-api.sumsub.com/idensic/static/sumsub-kyc.js',
+      `https://${
+        environment === 'production' ? '' : 'test-'
+      }api.sumsub.com/idensic/static/sumsub-kyc.js`,
     );
 
     areAssetsLoaded = true;
