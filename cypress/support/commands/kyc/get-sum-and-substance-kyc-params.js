18454f3eb6b4a95edc4674194b5c20c4ce337452
Dev <dev@0v1se.com>
2019-02-25 13:47:14 +0300

    Update Sum&Substance API key to fix E2E tests

--- cypress/support/commands/kyc/get-sum-and-substance-kyc-params.js
+++ cypress/support/commands/kyc/get-sum-and-substance-kyc-params.js
@@ -5,7 +5,7 @@ Cypress.Commands.add('getSumAndSubstanceKycParams', () => {
       provider: 'SUMSUB',
       sumsub: {
         clientId: 'Daonomic',
-        key: 'MASKDVRKNKNERN',
+        key: 'kKxUOUmb4WdAeZ6n2ASOaGlh0DmUgixl1Dy/RNsp1FsVBgQpbMA/3Q==',
         registration: 'OPTIONAL',
         userData: true,
       },
