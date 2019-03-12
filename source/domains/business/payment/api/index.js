5b016cb88e140b627117d4e26b932a34d7a878f5
Dev <dev@0v1se.com>
2019-03-12 13:54:48 +0300

    [495] Remove payments list

--- source/domains/business/payment/api/index.js
+++ source/domains/business/payment/api/index.js
@@ -1,8 +1,6 @@
 // @flow
 import { getPaymentAddress } from './get-payment-address';
-import { getPaymentStatus } from './get-payment-status';
 
 export const paymentApi = {
   getPaymentAddress,
-  getPaymentStatus,
 };
